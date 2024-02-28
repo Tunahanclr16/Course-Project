1ab8228a1572c4876bd0a41cc288ed329230a405
Tunahanclr16 <tunahancelerr16@gmail.com>
2024-02-28 13:27:07 +0300

    topbar gap fix

--- src/layout/TopBar.jsx
+++ src/layout/TopBar.jsx
@@ -6,7 +6,7 @@ export default function TopBar() {
   return (
     <div className="content  bg-orange-50 h-[55px] rounded-full  ">
         <div className="flex justify-center items-center h-full">
-              <h1 className="text-[#FFFFFF] font-normal flex items-center gap-4 text-sm  text-center">
+              <h1 className="text-[#FFFFFF] font-normal flex items-center gap-2 sm:gap-4 text-sm  text-center">
               Free Courses 🌟 Sale Ends Soon, Get It Now <FaArrowRightLong size={16} color="white" />
               </h1>
         </div>
