babc1cc4c5126d0b6268d20e11848644331508c7
Tunahanclr16 <tunahancelerr16@gmail.com>
2024-02-29 17:29:22 +0300

    api start pageIntroduction finish

---
+++ src/components/Home/Courses.jsx
@@ -0,0 +1,10 @@
+import PageIntroduction from "../../ui/PageIntroduction";
+
+export default function Courses() {
+  return (
+    <div className=" mt-[50px] sm:mt-[100px]">
+            <PageIntroduction title={"Courses"} description={"Lorem ipsum dolor sit amet consectetur. Tempus tincidunt etiam eget elit id imperdiet et. Cras eu sit dignissim lorem nibh et. Ac cum eget habitasse in velit fringilla feugiat senectus in."}/>
+
+    </div>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import Benefits from "../components/Home/Benefits";
 import Brands from "../components/Home/Brands";
+import Courses from "../components/Home/Courses";
 import Hero from "../components/Home/Hero";
 import HeroPhoto from "../components/Home/HeroPhoto";
 
 export default function Home() {
-  return <div>
+  return <div className=" lg:w-[1200px] w-[300px] xs:w-[360px] sm:w-[650px]  mx-auto mt-[50px] sm:mt-[100px]">
     <Hero/>
     <Brands/>
     <HeroPhoto/>
     <Benefits/>
+    <Courses/>
   </div>;
 }
