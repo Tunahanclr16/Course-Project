872966a320748c1a56ac508c3f42992f4e92ba9d
Tunahanclr16 <tunahancelerr16@gmail.com>
2024-04-03 15:30:39 +0300

    section header update

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,10 +1,7 @@
-import PricingCard from "../components/pricing/Pricing"
-import Accordion from "../components/accordion/Accordion"
+import SectionHeader from "../ui/SectionHeader";
+
 export default function AboutUs() {
-  return (
-    <div className="lg:max-w-[1596px] w-full mx-auto ">
-      <PricingCard />
-      <Accordion />
-    </div>
-  );
+  return <div className="lg:max-w-[1596px] w-full mx-auto ">
+    <SectionHeader title={"About Skillbridge"} description={"Welcome to our platform, where we are passionate about empowering individuals to master the world of design and development. We offer a wide range of online courses designed to equip learners with the skills and knowledge needed to succeed in the ever-evolving digital landscape."}/>
+  </div>;
 }
--- src/ui/SectionHeader.jsx
+++ src/ui/SectionHeader.jsx
@@ -2,15 +2,16 @@ import { NavLink } from "react-router-dom";
 
 export default function SectionHeader({ title, description }) {
   return (
-    <div className="flex lg:mt-[108px] mt-[50px] sm:mt-[100px] flex-col  justify-between">
-      <div>
+    <div className="flex px-2 sm:flex-row flex-col lg:mt-[108px] mt-[50px] sm:mt-[100px] justify-between">
+      <div className="sm:mx-auto">
         <h2 className="font-semibold md:p-2 lg:text-[48px] sm:text-[38px] text-[28px]">
           {title}
         </h2>
-        <div className="lg:w-[1177px]  sm:leading-6 md:leading-7 w-auto sm:w-[600px] md:w-[800px]  lg:mt-[6px] md:p-2 font-light mt-1 lg:text-[18px] sm:text-[16px] text-[14px]">
+       
+      </div>
+      <div className="lg:w-[1177px]  sm:leading-6 md:leading-7 w-auto sm:mx-auto sm:w-[600px] md:w-[800px]  lg:mt-[6px] md:p-2 font-light mt-1 lg:text-[18px] sm:text-[16px] text-[14px]">
           {description}
         </div>
-      </div>
     </div>
   );
 }
