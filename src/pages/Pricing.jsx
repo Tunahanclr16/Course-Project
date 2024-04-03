9d790ff8e8330acd4ac934c4db02c56f977e11ea
Tunahanclr16 <tunahancelerr16@gmail.com>
2024-04-03 15:24:58 +0300

    about us page delete finish pricing page

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,8 +1,10 @@
-
+import PricingCard from "../components/pricing/Pricing";
+import Accordion from "../components/accordion/Accordion";
 export default function Pricing() {
   return (
-    <div>
-    
+    <div className="lg:max-w-[1596px] w-full mx-auto ">
+      <PricingCard />
+      <Accordion />
     </div>
-  )
+  );
 }
